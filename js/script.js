73070422081c2be58bfc04d47cdfda11119acc19
AbdusattarovJurabek <jorabekaql07@gmail.com>
2023-03-04 21:01:26 +0500

    First comment

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use stript";
+
+const numberOfSeries = prompt("Nechta serial ko'rdingiz");
+
+
+const serialDB = {
+    count : numberOfSeries,
+    series : { },
+    actors : { },
+    genders : [],
+    private : false
+
+}
+
+const a = prompt("Oxirgi ko’rgan serialingiz?");
+const b = prompt("Nechi baxo berasiz?");
+const c = prompt("Oxirgi ko’rgan serialingiz?");
+const d = prompt("Nechi baxo berasiz?");
+    
+
+serialDB.series[a] = b;
+serialDB.series[c] = d;
+
+console.log(serialDB);
+
+
